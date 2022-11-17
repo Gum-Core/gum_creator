9a28c03b531cd0e7f34a1409e337b305b3ead9f1
iiRedDev <allanletalec@outlook.fr>
2022-11-17 11:39:36 +0100

    gum_creator

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game "rdr3"
+exports {
+	'loading',
+}
+client_scripts {
+	'config.lua',
+	'full_database.lua',
+	'client.lua',
+}
+
+server_scripts {
+	'full_database.lua',
+}
+ui_page 'html/index.html'
+
+files {
+	'html/*',
+}
